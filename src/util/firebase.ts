a0566aa4aa370bd5b538214bc41406844b6c6756
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-03-25 16:26:38 +1000

    getting ready for deployment

--- src/util/firebase.ts
+++ src/util/firebase.ts
@@ -9,13 +9,13 @@ import { browserSessionPersistence, getAuth, setPersistence } from "firebase/aut
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId,
+  apiKey: import.meta.env.apiKey,
+  authDomain: import.meta.env.authDomain,
+  projectId: import.meta.env.projectId,
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.env.appId,
+  measurementId: import.meta.env.measurementId,
 };
 
 // Initialize Firebase
--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,5 @@ export default defineConfig({
       start_url: '.',
       icons: []
     }})],
+    base: '/'
 })
